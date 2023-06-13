7b5afc98f202ac65bae03b82c3928eb7617031ed
mar1ase <lupita31311@gmail.com>
2023-06-13 00:30:30 -0500

    updated caresheet

--- script.js
+++ script.js
@@ -28,8 +28,40 @@ main.append(leftPnl)
 
 const middlePnl = document.createElement("div")
 middlePnl.classList.add("middlepnl")
-main.append(middlePnl)
+const topDiv = document.createElement("div")
+topDiv.classList.add("topDiv")
+topDiv.innerHTML = `<p>You just adopted/rescued your first dog? or maybe even purchased your pup. It's always an exciting time to bring a new family member home, but dogs are a huge commitment. They require lots of care and attention. Here is a start up list to help you with basic necessities. </p> 
+                    <br>`
+const list = document.createElement("div")
+list.classList.add("list")
+list.innerHTML = `<ul>
+                    <li>Complete and balanced, age/breed appropriate dog food</li>  
+                    <li>Food and water bowls (preferably stainless steel material)</li>  
+                    <li>collar and leash</li>  
+                    <li>ID tag</li>
+                    <li>kennel and/or doghouse</li>  
+                    <li>toys age and size appropriate</li>  
+                    <li>training treats</li>  
+                    <li>grooming supplies</li>
+                    <li>dental hygiene supplies</li>
+                    <li>kennel bed and/or mat</li>
+                    <li>flea & tick preventive</li>
+                    <li>Dog safe surface cleaners</li>
+                  </ul>
+                  <br>
+                  <p>Some other things are optional such as: Dog clothes, Dog multivitamins, and depending if, how,  and where you're potty training your new dog you might need potty training pads. </p>`
+topDiv.append(list)
 
+const middleDiv = document.createElement("div")
+middleDiv.classList.add("middleDiv")
+const bottomDiv = document.createElement("div")
+bottomDiv.classList.add("bottomDiv")
+
+middlePnl.append(topDiv)
+middlePnl.append(middleDiv)
+middlePnl.append(bottomDiv)
+
+main.append(middlePnl)
 
 const rightPnl = document.createElement("div")
 rightPnl.classList.add("rightpnl")
