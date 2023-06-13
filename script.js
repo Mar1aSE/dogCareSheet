4bfddeb70e08f8de5e92deda3e854a6cb9830759
mar1ase <lupita31311@gmail.com>
2023-06-13 02:24:01 -0500

    adding more content

--- script.js
+++ script.js
@@ -37,15 +37,16 @@ list.classList.add("list")
 list.innerHTML = `<ul>
                     <li>Complete and balanced, age/breed appropriate dog food</li>  
                     <li>Food and water bowls (preferably stainless steel material)</li>  
-                    <li>collar and leash</li>  
+                    <li>Collar</li>  
                     <li>ID tag</li>
-                    <li>kennel and/or doghouse</li>  
-                    <li>toys age and size appropriate</li>  
-                    <li>training treats</li>  
-                    <li>grooming supplies</li>
-                    <li>dental hygiene supplies</li>
-                    <li>kennel bed and/or mat</li>
-                    <li>flea & tick preventive</li>
+                    <li>Harness and leash</li>  
+                    <li>Kennel and/or doghouse</li>  
+                    <li>Toys age and size appropriate</li>  
+                    <li>Training treats</li>  
+                    <li>Grooming supplies</li>
+                    <li>Dental hygiene supplies</li>
+                    <li>Kennel bed and/or mat</li>
+                    <li>Flea & tick preventive</li>
                     <li>Dog safe surface cleaners</li>
                   </ul>
                   <br>
@@ -54,8 +55,26 @@ topDiv.append(list)
 
 const middleDiv = document.createElement("div")
 middleDiv.classList.add("middleDiv")
+middleDiv.innerHTML = `<p>Whether you got a puppy or an older dog knowing the right feeding portions is important to keep your dog healthy.</p> 
+    <br> 
+    <ul>
+        <li>Puppies eight to 12 weeks old need four meals a day.</li>
+        <li>Puppies three to six months old need 3 meals a day.</li>
+        <li>Puppies six monthst to one year need two meals a day</li>
+        <li>Once your dog reaches the one year old mark depending on the size of the feeding 1-2 times per day.</li>
+        <li>If your dog is prone to bloating or is a large canine, two smaller meals a day are better</li>
+     </ul>
+     <br>
+     <h5>Dry dog food may be mixed with water, broth or canned food </h5>`
+
 const bottomDiv = document.createElement("div")
 bottomDiv.classList.add("bottomDiv")
+bottomDiv.innerHTML = `<p>There is so much more to learn when it comes to owning a dog and making sure you do your research on the breed your dog is, it will make it so much easier on learning temperament and needs. Vaccinations are needed on any dog as well as constant grooming. Giving your dog a proper happy life style is the goal. Female dogs should be spayed and male dogs neutered by six months of age. Most importantly don't forget to do as much research as you can on the breed and give tons of buttrubs and kisses to your furry friend. </p>
+<br> 
+<h5>If you suspect your dog has ingested a poisonous substance, call your veterinarian or call the pet poison helpline 24/7 at
+<a href="tel:800-213-6680">800-213-6680</a> </h5>
+<br>
+<h6>~ Some of my favorite must haves in the links below ~</h6`
 
 middlePnl.append(topDiv)
 middlePnl.append(middleDiv)
